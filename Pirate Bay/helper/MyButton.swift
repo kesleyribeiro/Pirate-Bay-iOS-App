41817c38de9d4235049001f83dadbc6c5a473edd
Kesley Ribeiro <kesley.developer@icloud.com>
2018-05-11 16:10:19 -0300

    Custom button - @IBDesignable and @IBInspectable

---
+++ Pirate Bay/helper/MyButton.swift
@@ -0,0 +1,23 @@
+//
+//  MyButton.swift
+//  Pirate Bay
+//
+//  Created by Kesley Ribeiro on 11/May/18.
+//  Copyright © 2018 Kesley Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class MyButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0.0
+    @IBInspectable var borderWidth: CGFloat = 0.0
+    @IBInspectable var borderColor: UIColor = UIColor.clear
+    
+    override func draw(_ rect: CGRect) {
+        self.layer.cornerRadius = cornerRadius
+        self.layer.borderWidth = borderWidth
+        self.layer.borderColor = borderColor.cgColor
+    }
+}
