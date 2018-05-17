3a62c331d03c0eb010937d147b2217a782d84992
Kesley Ribeiro <kesley.developer@icloud.com>
2018-05-17 15:12:59 -0300

    Sign In layout UI

---
+++ Pirate Bay/Controller/login/LoginVC.swift
@@ -0,0 +1,37 @@
+//
+//  LoginVC.swift
+//  Pirate Bay
+//
+//  Created by Kesley Ribeiro on 17/May/18.
+//  Copyright © 2018 Kesley Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var emailTxtField: UITextField!
+    @IBOutlet weak var passwordTxtField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func didTapCancel(_ sender: Any) {
+   
+        
+    }
+    
+    @IBAction func didTapSignIn(_ sender: MyButton) {
+    
+        
+    }
+    
+    
+}
