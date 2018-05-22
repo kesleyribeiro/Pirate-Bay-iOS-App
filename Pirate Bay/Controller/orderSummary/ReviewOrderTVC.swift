5607aa2b21fc8f66333e257a6feca25c45f1be98
Kesley Ribeiro <kesley.developer@icloud.com>
2018-05-22 16:18:44 -0300

    Review order layout UI

---
+++ Pirate Bay/Controller/orderSummary/ReviewOrderTVC.swift
@@ -0,0 +1,32 @@
+//
+//  ReviewOrderTVC.swift
+//  Pirate Bay
+//
+//  Created by Kesley Ribeiro on 22/May/18.
+//  Copyright © 2018 Kesley Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class ReviewOrderTVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+    
+}
---
+++ Pirate Bay/view/orderSummary/ShippingTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShippingTVCell.swift
+//  Pirate Bay
+//
+//  Created by Kesley Ribeiro on 22/May/18.
+//  Copyright © 2018 Kesley Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class ShippingTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
